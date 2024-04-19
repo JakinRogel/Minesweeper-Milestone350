283ddfe584ee1a1ea4d4bb7863921c0e8886fa23
Miguel Gallegos <94002329+mgalleg93@users.noreply.github.com>
2024-04-19 16:41:35 -0700

    Now merge

---
+++ Models/ButtonModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Minesweeper_Milestone350.Models
+{
+    public class ButtonModel
+    {
+        public int Id { get; set; }
+
+        public int ButtonState { get; set; }
+
+        public ButtonModel()
+        {
+        }
+
+        public ButtonModel(int id, int buttonState)
+        {
+            Id=id;
+            ButtonState=buttonState;
+        }
+       
+    }
+}
--- obj/Debug/net8.0/Minesweeper-Milestone350.AssemblyInfo.cs
+++ obj/Debug/net8.0/Minesweeper-Milestone350.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper-Milestone350")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a3636c456a43d97c6edd4229c12769902ce13e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4954ce486ac3db2a8c4c0df872b53c6f68028c17")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper-Milestone350")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper-Milestone350")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
